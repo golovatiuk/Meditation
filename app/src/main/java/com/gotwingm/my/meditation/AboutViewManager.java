dc59fde07bdfb6932706536087b7cd434909c490
Nickolay <gotwingm@gmail.com>
2015-08-07 16:28:19 +0300

    final 07/08
    final 07/08

--- app/src/main/java/com/gotwingm/my/meditation/AboutViewManager.java
+++ app/src/main/java/com/gotwingm/my/meditation/AboutViewManager.java
@@ -1,6 +1,5 @@
 package com.gotwingm.my.meditation;
 
-import android.text.method.ScrollingMovementMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AnimationUtils;
@@ -13,13 +12,11 @@ import android.widget.ViewFlipper;
 
 public class AboutViewManager extends MainActivity implements View.OnClickListener, View.OnTouchListener {
 
-    static float fromX;
-    static float fromY;
-    static float toX;
-    static float toY;
-    ViewFlipper aboutViewFlipper;
-    View linksView;
-    View textView;
+    protected ViewFlipper aboutViewFlipper;
+
+    private static float fromX;
+    private View linksView;
+    private View textView;
 
     AboutViewManager() {
 
@@ -86,7 +83,6 @@ public class AboutViewManager extends MainActivity implements View.OnClickListen
     public void makeAboutView() {
 
         mainViewFlipper.addView(aboutView);
-
         aboutViewFlipper.addView(textView);
         aboutViewFlipper.addView(linksView);
         aboutViewFlipper.addView(layoutInflater.inflate(R.layout.about_credits, null));
@@ -101,96 +97,73 @@ public class AboutViewManager extends MainActivity implements View.OnClickListen
         {
             case MotionEvent.ACTION_DOWN:
                 fromX = event.getX();
-                fromY = event.getY();
                 break;
 
             case MotionEvent.ACTION_MOVE:
-                toX = event.getX();
-                toY = event.getY();
-
-                if ((Math.abs(fromX - toX)) < (Math.abs(fromY - toY)))
-                {
-
-                    ((TextView) textView.findViewById(R.id.aboutText)).setMovementMethod(new ScrollingMovementMethod());
-                }
                 break;
 
             case MotionEvent.ACTION_UP:
-                toX = event.getX();
+                final float toX = event.getX();
 
                 if (fromX > toX) {
-
                     if (aboutViewFlipper.getDisplayedChild() == 2) {
-
                         break;
-
                     }
 
                     setNextIndicator(aboutViewFlipper.getDisplayedChild());
-
                     aboutViewFlipper.setInAnimation(AnimationUtils.loadAnimation(context, R.anim.go_next_in));
                     aboutViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(context,R.anim.go_next_out));
                     aboutViewFlipper.showNext();
 
                 } else if (fromX < toX) {
-
                     if (aboutViewFlipper.getDisplayedChild() == 0) {
-
                         break;
-
                     }
 
                     setPreviousIndicator(aboutViewFlipper.getDisplayedChild());
                     aboutViewFlipper.setInAnimation(AnimationUtils.loadAnimation(context, R.anim.go_prev_in));
                     aboutViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(context, R.anim.go_prev_out));
                     aboutViewFlipper.showPrevious();
-
                 }
 
-            default:
-                break;
-
+            default: break;
         }
-
         return true;
     }
 
     private void setNextIndicator(int index) {
 
         switch (index) {
-
             case 0:
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.indicator_active);
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator1)).setImageResource(R.drawable.indicator);
-                break;
-            case 1:
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator3)).setImageResource(R.drawable.indicator_active);
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.indicator);
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.about_indicator_active);
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator1)).setImageResource(R.drawable.about_indicator);
                 break;
 
+            case 1:
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator3)).setImageResource(R.drawable.about_indicator_active);
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.about_indicator);
+                break;
         }
     }
 
     private void setPreviousIndicator(int index) {
 
         switch (index) {
-
             case 2:
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.indicator_active);
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator3)).setImageResource(R.drawable.indicator);
-                break;
-            case 1:
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator1)).setImageResource(R.drawable.indicator_active);
-                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.indicator);
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.about_indicator_active);
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator3)).setImageResource(R.drawable.about_indicator);
                 break;
 
+            case 1:
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator1)).setImageResource(R.drawable.about_indicator_active);
+                ((ImageView) aboutView.findViewById(R.id.aboutBottomBarIndicator2)).setImageResource(R.drawable.about_indicator);
+                break;
         }
     }
 
     public void openLinks(int index, int id) {
 
         String[] linksArray = context.getResources().getStringArray(R.array.links);
-
         View browserView = layoutInflater.inflate(R.layout.browser_view, null);
 
         mainViewFlipper.addView(browserView);
@@ -199,7 +172,6 @@ public class AboutViewManager extends MainActivity implements View.OnClickListen
 
         WebView webView = (WebView) browserView.findViewById(R.id.browserWebView);
         ((TextView) browserView.findViewById(R.id.browserTitle)).setText(((TextView) linksView.findViewById(id)).getText());
-
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.setWebViewClient(new MyBrowser());
         webView.loadUrl(linksArray[index]);
@@ -214,4 +186,5 @@ public class AboutViewManager extends MainActivity implements View.OnClickListen
             return true;
         }
     }
+
 }
